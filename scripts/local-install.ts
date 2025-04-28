f6a3a768f74ec6f02b6e841f9b2a9a7c90891839
bezalel6 <bezalel3250@gmail.com>
2025-04-28 21:02:44 +0300

    Local install script, Dynamic icons

---
+++ scripts/local-install.ts
@@ -0,0 +1,19 @@
+import { version } from '../package.json'
+import { execSync } from 'child_process'
+
+const extensionPath = `tickify-${version}.vsix`
+import * as fs from 'fs'
+
+// Check if the VSIX file already exists
+if (fs.existsSync(extensionPath)) {
+    // get around vscode's issue with installing an existing version
+    console.log(`This version exists already`)
+}else{
+    // Run the build and package process
+    execSync('vsce package', { stdio: 'inherit' })
+    
+    // Install the extension
+    execSync(`code --install-extension ${extensionPath}`, { stdio: 'inherit' })
+
+}
+
